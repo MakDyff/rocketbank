3518a1563c1621fc8273b53e8bff52959827767b
levashovm.s <levashovm.s@gmail.com>
2019-01-21 23:19:29 +0300

    методы, скорость

--- app/src/main/java/views/CustomImageView.kt
+++ app/src/main/java/views/CustomImageView.kt
@@ -1,47 +1,46 @@
 package views
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.LinearLayout
 import models.ImageModel
-import android.widget.ImageView
 import ru.makdyff.rocketbank.R
 import android.graphics.*
 import android.graphics.Bitmap
-import android.widget.Toast
-import android.R.attr.y
-import android.R.attr.x
-import android.R.attr.bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
+import android.widget.*
+import java.util.concurrent.Future
 
 
+class CustomImageView : ViewBase {
 
-
-
-class CustomImageView: ViewBase {
-
-    private var _model = ImageModel(Array(0){Array(0) { i -> i} })
+    private var _model = ImageModel(Array(0) { Array(0) { i -> i } })
     private val _img: ImageView
+    private val _bar: SeekBar
+    private val _methods: Spinner
+    private var _future: Future<*>? = null
+    private var _bitmap: Bitmap? = null
 
     var model: ImageModel
         get() = _model
-        set(value) { _model = value }
+        set(value) {
+            _model = value
+        }
 
     constructor(context: Context?) : super(context) {}
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) { }
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
 
     init {
         LinearLayout.inflate(context, R.layout.view_image, this)
-        _img = findViewById(ru.makdyff.rocketbank.R.id.view_image_id)
+        _img = findViewById(R.id.view_image_id)
+        _bar = findViewById(R.id.view_image_bar)
+        _methods = findViewById(R.id.view_image_methods)
+    }
 
-
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
 
         _img.setOnTouchListener { v, event ->
-            val imgDrawable = (v as ImageView).drawable
-            //imgDrawable will not be null if you had set src to ImageView, in case of background drawable it will be null
-            val bitmap = (imgDrawable as BitmapDrawable).bitmap
-
             val inverse = Matrix()
             (v as ImageView).imageMatrix.invert(inverse)
             val touchPoint = floatArrayOf(event.x, event.y)
@@ -49,37 +48,116 @@ class CustomImageView: ViewBase {
             val xCoord = touchPoint[0].toInt()
             val yCoord = touchPoint[1].toInt()
 
-            val touchedRGB = bitmap.getPixel(xCoord, yCoord)
-
             Toast.makeText(context, "x ${xCoord} y ${yCoord}", Toast.LENGTH_LONG).show()
+
+            start(xCoord, yCoord)
 
             true
         }
     }
 
+    /*
+    Выбор типа раскраки и начало выполнения
+     */
+    private fun start(x: Int, y: Int) {
+//        _future?.cancel(false)
+
+        _future = pool.submit {
+            val method =  _methods.selectedItemPosition
+            when(method) {
+                0 -> horz(_model.mass[x][y], x, y)
+                1 -> vert(_model.mass[x][y], x, y)
+                else -> {
+
+                }
+            }
+
+            post {
+                Toast.makeText(context, "Finish", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    /*
+     * Горизонтальный метод
+     */
+    private fun horz(v: Int, x: Int, y: Int) {
+        if (x >= 0
+            && _model.mass.size > x
+            && y >= 0
+            && _model.mass[x].size > y
+            && _model.mass[x][y] == v
+        ) {
+            _model.mass[x][y] = 2
+            timeChange(x,y)
+
+            horz(v, x - 1, y)
+            horz(v, x + 1, y)
+            horz(v, x, y - 1)
+            horz(v, x, y + 1)
+        }
+    }
+
+    /*
+     * Вертикальный метод
+     */
+    private fun vert(v: Int, x: Int, y: Int) {
+        if (x >= 0
+            && _model.mass.size > x
+            && y >= 0
+            && _model.mass[x].size > y
+            && _model.mass[x][y] == v
+        ) {
+            _model.mass[x][y] = 2
+            timeChange(x,y)
+
+            vert(v, x, y - 1)
+            vert(v, x, y + 1)
+            vert(v, x - 1, y)
+            vert(v, x + 1, y)
+        }
+    }
+
+    /*
+    Ромб
+     */
+    private fun tmp1() {
+
+    }
+
+    private fun timeChange(x: Int, y: Int) {
+        _bitmap?.setPixel(x, y, Color.RED);
+        post{
+            _img.setImageBitmap(_bitmap)
+        }
+        val time = _bar.progress.toLong()
+        Thread.sleep(time)
+    }
+
     override fun notifyDataSetChanged() {
         post {
-            val bitmap = Bitmap.createBitmap(
+            _bitmap = Bitmap.createBitmap(
                 _model.mass.size, _model.mass[0].size,
                 Bitmap.Config.ARGB_8888
             )
-            _img.setImageBitmap(bitmap)
 
             var x = -1
-            for(m in _model.mass) {
+            for (m in _model.mass) {
                 x++
                 var y = -1
-                for(c in m) {
-                   y++
-                    if(c == 2) {
-                        bitmap?.setPixel(x, y, Color.RED);
-                    } else if(c == 1) {
-                        bitmap?.setPixel(x, y, Color.BLACK);
+                for (c in m) {
+                    y++
+                    if (c == 2) {
+                        _bitmap?.setPixel(x, y, Color.RED);
+                    } else if (c == 1) {
+                        _bitmap?.setPixel(x, y, Color.BLACK);
                     } else {
-                        bitmap?.setPixel(x, y, Color.WHITE);
+                        _bitmap?.setPixel(x, y, Color.WHITE);
                     }
                 }
             }
+
+            _img.setImageBitmap(_bitmap)
         }
     }
 
